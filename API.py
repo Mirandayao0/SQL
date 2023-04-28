58db0478a094dd273782a446169fbb4400c69428
Mirandayao0 <yac97@pitt.edu>
2023-04-28 14:45:04 -0400

    Update API.py

--- API.py
+++ API.py
@@ -10,7 +10,6 @@ db = pymysql.connect(
     )
 cursor = db.cursor()
 
-
 @app.route('/positions', methods=['GET'])
 def get_positions():
     # Execute the SELECT query
@@ -29,6 +28,44 @@ def get_positions():
     # Return the results as JSON
     return jsonify(results)
 
+@app.route('/positions', methods=['POST'])
+def insert_positions():
+    # parse the incoming JSON data
+    data = request.json
+
+    # validate the data
+    if 'name' not in data:
+        return 'Invalid data', 400
+
+    # insert the data into the MySQL database
+    query = 'INSERT INTO Positions (name) VALUES (%s)'
+    cursor.execute(query, (data['name']))
+    # db.commit()
+
+    return 'Positions Data inserted successfully', 201
+
+@app.route('/positions/<index>', methods=['DELETE'])
+def delete_positions(index):
+    # delete the data from the MySQL database
+    query = 'DELETE FROM Positions WHERE position_id = %s'
+    cursor.execute(query, index)
+    return 'Positions Data deleted successfully', 204
+
+@app.route('/positions/<index>', methods=['UPDATE'])
+def update_positions(index):
+    # parse the incoming JSON data
+    data = request.json
+    # validate the data
+    if 'name' not in data :
+        return 'Invalid data', 400
+
+    # insert the data into the MySQL database
+    query = 'UPDATE Positions SET name = %s WHERE position_id = %s'
+    cursor.execute(query, (data['name'],index))
+    # db.commit()
+
+    return 'Positions Data update successfully', 201
+    
 
 @app.route('/customers', methods=['GET'])
 def get_customers():
@@ -54,6 +91,47 @@ def get_customers():
     # Return the results as JSON
     return jsonify(results)
 
+
+@app.route('/customers', methods=['POST'])
+def insert_customers():
+    # parse the incoming JSON data
+    data = request.json
+
+    # validate the data
+    if 'name' not in data or 'email' not in data or 'phone' not in data or 'gender' not in data or 'DOB' not in data or 'join_date' not in data or 'membership_type' not in data:
+        return 'Invalid data', 400
+
+    # insert the data into the MySQL database
+    query = 'INSERT INTO Customers (name, email, phone,gender,date_of_birth,join_date,membership_type) VALUES (%s, %s, %s,%s,%s,%s,%s)'
+    cursor.execute(query, (data['name'], data['email'], data['phone'],data['gender'],data['DOB'],data['join_date'],data['membership_type']))
+    # db.commit()
+
+    return 'Customers Data inserted successfully', 201
+
+@app.route('/customers/<index>', methods=['DELETE'])
+def delete_customers(index):
+    # delete the data from the MySQL database
+    query = 'DELETE FROM Customers WHERE customer_id = %s'
+    cursor.execute(query, index)
+    return 'Customers Data deleted successfully', 204
+
+@app.route('/customers/<index>', methods=['UPDATE'])
+def update_customers(index):
+    # parse the incoming JSON data
+    data = request.json
+    # validate the data
+    if 'name' not in data or 'email' not in data or 'phone' not in data or 'gender' not in data or 'DOB' not in data or 'join_date' not in data or 'membership_type' not in data:
+        return 'Invalid data', 400
+
+    # insert the data into the MySQL database
+    query = 'UPDATE Customers SET name = %s, email = %s, phone = %s,gender = %s,date_of_birth = %s,join_date = %s, membership_type = %s WHERE customer_id = %s'
+    cursor.execute(query, (data['name'], data['email'], data['phone'],data['gender'],data['DOB'],data['join_date'],data['membership_type'], index))
+    # db.commit()
+
+    return 'Customers Data update successfully', 201
+    
+
+
 @app.route('/services', methods=['GET'])
 def get_services():
     # Execute the SELECT query
@@ -74,6 +152,47 @@ def get_services():
     # Return the results as JSON
     return jsonify(results)
 
+@app.route('/services', methods=['POST'])
+def insert_services():
+    # parse the incoming JSON data
+    data = request.json
+
+    # validate the data
+    if 'name' not in data or 'description' not in data or 'price_per_session' not in data:
+        return 'Invalid data', 400
+
+    # insert the data into the MySQL database
+    query = 'INSERT INTO Services (name, description, price_per_session) VALUES (%s, %s, %s)'
+    cursor.execute(query, (data['name'], data['description'], data['price_per_session']))
+    # db.commit()
+
+    return 'Services Data inserted successfully', 201
+
+@app.route('/services/<index>', methods=['DELETE'])
+def delete_services(index):
+    # delete the data from the MySQL database
+    query = 'DELETE FROM Services WHERE service_id = %s'
+    cursor.execute(query, index)
+    return 'Services Data deleted successfully', 204
+
+@app.route('/services/<index>', methods=['UPDATE'])
+def update_services(index):
+    # parse the incoming JSON data
+    data = request.json
+
+    # validate the data
+    if 'name' not in data or 'description' not in data or 'price_per_session' not in data:
+        return 'Invalid data', 400
+
+    # insert the data into the MySQL database
+    query = 'UPDATE Services SET name = %s, description = %s, price_per_session = %s WHERE service_id = %s'
+    cursor.execute(query, (data['name'], data['description'], data['price_per_session'], index))
+    # db.commit()
+
+    return 'Services Data update successfully', 201
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
+
